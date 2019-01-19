9f210d7f1b238fc2eeef990e1091495647e4ae47
wat1r <cnwangzhou@hotmail.com>
2019-01-19 23:09:33 +0800

    Test Two

--- basic-tech-core/src/main/java/utils/TestTwo.java
+++ basic-tech-core/src/main/java/utils/TestTwo.java
@@ -1,4 +1,19 @@
 package utils;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TestTwo {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(TestTwo.class);
+
+    public static void main(String[] args) {
+        LOGGER.debug("debug");
+        LOGGER.info("info");
+        LOGGER.error("error");
+
+
+    }
+
 }
