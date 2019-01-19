3177ebe70256a0586d9ac0575fd7bee73db221f7
wat1r <cnwangzhou@hotmail.com>
2019-01-19 23:14:00 +0800

    guava init

--- basic-tech-core/src/main/java/utils/TestTwo.java
+++ basic-tech-core/src/main/java/utils/TestTwo.java
@@ -1,6 +1,8 @@
 package utils;
 
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +15,8 @@ public class TestTwo {
         LOGGER.info("info");
         LOGGER.error("error");
 
+
+        LOGGER.info(JSON.toJSONString(Lists.newArrayList("hello","test")));
 
     }
 
