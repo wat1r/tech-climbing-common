f187582087a83d82207c81cafb91f7c4d0c49855
wat1r <cnwangzhou@hotmail.com>
2019-01-20 23:34:24 +0800

    1.IO

---
+++ basic-tech-core/src/main/test/io/importNew/SocketChannelTest.java
@@ -0,0 +1,34 @@
+package io.importNew;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+/**
+ * Created by FrankCooper
+ * Date 2019/1/20 23:30
+ * Description
+ */
+public class SocketChannelTest {
+    public static void main(String[] args) throws IOException {
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.connect(new InetSocketAddress("localhost", 8080));
+
+        // 发送请求
+        ByteBuffer buffer = ByteBuffer.wrap("1234567890".getBytes());
+        socketChannel.write(buffer);
+
+        // 读取响应
+        ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+        int num;
+        if ((num = socketChannel.read(readBuffer)) > 0) {
+            readBuffer.flip();
+            byte[] re = new byte[num];
+            readBuffer.get(re);
+
+            String result = new String(re, "UTF-8");
+            System.out.println("返回值: " + result);
+        }
+    }
+}
