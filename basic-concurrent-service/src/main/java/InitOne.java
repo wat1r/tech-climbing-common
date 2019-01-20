070236aad0b564b67d1da7139ea3a00014f6b7c3
wat1r <cnwangzhou@hotmail.com>
2019-01-20 10:24:25 +0800

    maven-shade-plugin init

---
+++ basic-concurrent-service/src/main/java/InitOne.java
@@ -0,0 +1,22 @@
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
+import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InitOne {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(InitOne.class);
+    public static void main(String[] args) {
+
+
+        LOGGER.info(String.format("test the list:%s", JSON.toJSON(Lists.newArrayList("oo"))));
+
+
+
+
+
+    }
+
+
+}
