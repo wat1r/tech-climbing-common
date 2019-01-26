343d2971011ab2d80ee00509c2295400326fe2f1
wangzhou <wangzhou@patsnap.com>
2019-01-26 11:28:21 +0800

    1.test two

---
+++ basic-tech-core/src/main/java/io/TestTwo.java
@@ -0,0 +1,15 @@
+package io;
+
+/**
+ * @Author:Cooper
+ * @Date:2019/1/26 11:24
+ */
+public class TestTwo {
+
+    public static void main(String[] args) {
+//        System.out.printf("this is a %s","hello");
+
+
+        System.out.println("he");
+    }
+}
