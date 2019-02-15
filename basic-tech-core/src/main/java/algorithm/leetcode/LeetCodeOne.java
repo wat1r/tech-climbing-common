42a3f66aa34bcc58b32fd66db52ed107145de55a
wangzhou <wangzhou@patsnap.com>
2019-02-15 13:28:33 +0800

    1.leetcode

---
+++ basic-tech-core/src/main/java/algorithm/leetcode/LeetCodeOne.java
@@ -0,0 +1,26 @@
+package algorithm.leetcode;
+
+/**
+ * @Author:Cooper
+ * @Date:2019/2/15 11:43
+ */
+public class LeetCodeOne {
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+
+
+
+
+
+}
