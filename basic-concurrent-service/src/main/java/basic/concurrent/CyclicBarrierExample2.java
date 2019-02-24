092c5c45a355abc285bfaae63df0d64e723e1b0f
wat1r <cnwangzhou@hotmail.com>
2019-02-25 00:16:19 +0800

    1.Java Guide --- concurrent

---
+++ basic-concurrent-service/src/main/java/basic/concurrent/CyclicBarrierExample2.java
@@ -0,0 +1,47 @@
+package basic.concurrent;
+
+import java.util.concurrent.*;
+
+/**
+ * Created by FrankCooper
+ * Date 2019/2/25 0:09
+ * Description
+ */
+public class CyclicBarrierExample2 {
+    // 请求的数量
+    private static final int threadCount = 550;
+    // 需要同步的线程数量
+    private static final CyclicBarrier cyclicBarrier = new CyclicBarrier(5);
+
+    public static void main(String[] args) throws InterruptedException {
+        // 创建线程池
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
+        for (int i = 0; i < threadCount; i++) {
+            final int threadNum = i;
+            Thread.sleep(1000);
+            threadPool.execute(() -> {
+                try {
+                    test(threadNum);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            });
+        }
+        threadPool.shutdown();
+    }
+
+    public static void test(int threadnum) throws InterruptedException, BrokenBarrierException {
+        System.out.println("threadnum:" + threadnum + "is ready");
+        try {
+            cyclicBarrier.await(2000, TimeUnit.MILLISECONDS);
+        } catch (Exception e) {
+            System.out.println("-----CyclicBarrierException------");
+        }
+        System.out.println("threadnum:" + threadnum + "is finish");
+    }
+}
