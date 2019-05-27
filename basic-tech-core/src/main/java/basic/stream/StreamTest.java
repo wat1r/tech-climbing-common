a1b5d7aac49dd5f6d34c57b54d8f6781772bd77d
wat1r <cnwangzhou@hotmail.com>
2019-05-27 22:50:31 +0800

    Stream API

---
+++ basic-tech-core/src/main/java/basic/stream/StreamTest.java
@@ -0,0 +1,58 @@
+package basic.stream;
+
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
+
+/**
+ * Created by FrankCooper
+ * Date 2019/5/27 21:07
+ * Description
+ */
+public class StreamTest {
+
+
+    public static void main(String[] args) {
+
+//        testOne();
+        testTwo();
+    }
+
+
+    public static void testOne() {
+        List<List<String>> lists = new ArrayList<>();
+        lists.add(Arrays.asList("apple", "click"));
+        lists.add(Arrays.asList("boss", "dig", "qq", "vivo"));
+        lists.add(Arrays.asList("c#", "biezhi"));
+        long count = lists.stream().flatMap(Collection::stream).filter(str -> str.length() > 2).count();
+
+        System.out.println(count);
+        System.out.println("-----------------");
+
+        String[] dd = {"a", "b", "c"};
+        Stream<String> stream = Arrays.stream(dd);
+        stream.filter(str -> str.equals("a")).forEach(System.out::println);
+    }
+
+    public static void testTwo() {
+        Integer[] dd = {1, 2, 3};
+        Stream<Integer> stream = Arrays.stream(dd);
+        stream.map(str -> Integer.toString(str)).forEach(System.out::println);
+
+
+        String[] strs = { "aaa", "bbb", "ccc" };
+        Arrays.stream(strs).map(str -> str.split("")).forEach(System.out::println);// Ljava.lang.String;@53d8d10a
+        Arrays.stream(strs).map(str -> str.split("")).flatMap(Arrays::stream).forEach(System.out::println);// aaabbbccc
+        Arrays.stream(strs).map(str -> str.split("")).flatMap(str -> Arrays.stream(str)).forEach(System.out::println);// aaabbbccc
+
+        /**
+         * 第一段输出代码里，我们先看map操作，通过上面对map的介绍，我们可以看到，map可以改变返回的Stream的泛型，str.split("")，根据空字符串分隔，
+         * 返回的类型是一个数组，返回的流也是Stream<String[]>，而不是Stream<String>；在第二段代码中，数组的流，经过map操作，
+         * 返回Stream<String[]>后，再经过flatMap，把数组通过Arrays.stream变成一个新的流，再返回到原来的流里；
+         * 这样，两个流就合并成一个流；第三段代码，是第二段代码的，另一种写法；
+         */
+    }
+
+
+
+}
