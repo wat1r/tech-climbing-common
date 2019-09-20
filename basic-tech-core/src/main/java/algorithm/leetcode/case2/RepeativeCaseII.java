29fe67295995d6b71c8052e4ca26f6d13c97400f
wat1r <cnwangzhou@hotmail.com>
2019-09-20 21:33:26 +0800

    子序列

--- basic-tech-core/src/main/java/algorithm/leetcode/case2/RepeativeCaseII.java
+++ basic-tech-core/src/main/java/algorithm/leetcode/case2/RepeativeCaseII.java
@@ -2279,6 +2279,22 @@ public class RepeativeCaseII {
     }
 
 
+    public int findLengthOfLCIS(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);
+        int max = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                dp[i] = dp[i - 1] + 1;
+                max = Math.max(max, dp[i]);
+            }
+        }
+        return max;
+    }
+
+
     public static void main(String[] args) {
 //        int[] nums = {1, 2, 3};
 //        handler.subsets(nums);
