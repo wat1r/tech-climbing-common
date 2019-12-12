49ae1b06a866667da4fd4d7b74c4377d19379b26
wat1r <cnwangzhou@hotmail.com>
2019-12-12 23:13:30 +0800

    stack

--- basic-tech-core/src/main/java/algorithm/leetcode/case2/RepeativeCaseII.java
+++ basic-tech-core/src/main/java/algorithm/leetcode/case2/RepeativeCaseII.java
@@ -3787,6 +3787,81 @@ public class RepeativeCaseII {
     }
 
 
+    public void setZeroes1(int[][] matrix) {
+        int R = matrix.length;
+        int C = matrix[0].length;
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+
+    }
+
+
+
+    public void setZeroes(int[][] matrix) {
+        Boolean isCol = false;
+        int R = matrix.length;
+        int C = matrix[0].length;
+
+        for (int i = 0; i < R; i++) {
+
+            // Since first cell for both first row and first column is the same i.e. matrix[0][0]
+            // We can use an additional variable for either the first row/column.
+            // For this solution we are using an additional variable for the first column
+            // and using matrix[0][0] for the first row.
+            if (matrix[i][0] == 0) {
+                isCol = true;
+            }
+
+            for (int j = 1; j < C; j++) {
+                // If an element is zero, we set the first element of the corresponding row and column to 0
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        // Iterate over the array once again and using the first row and first column, update the elements.
+        for (int i = 1; i < R; i++) {
+            for (int j = 1; j < C; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // See if the first row needs to be set to zero as well
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < C; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // See if the first column needs to be set to zero as well
+        if (isCol) {
+            for (int i = 0; i < R; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
 //        int[] nums = {1, 2, 3};
 //        handler.subsets(nums);
@@ -3932,7 +4007,12 @@ public class RepeativeCaseII {
 //        handler.reverseParentheses1st("(u(love)i)");
 //        handler.findDuplicate2nd(new int[]{3, 1, 3, 4, 2});
 //        handler.subarraySum(new int[]{1, 1, 1}, 2);
-        handler.subarraySum2nd(new int[]{1, 1, 1}, 2);
+//        handler.subarraySum2nd(new int[]{1, 1, 1}, 2);
+
+        handler.setZeroes(new int[][]{{1,1,2,0},{3,4,5,2},{1,3,1,5}});
+
+
+
     }
 //winter
 
