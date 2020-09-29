1d734b89e75e81ca5d9ad13b4fc9298c4aa7a385
wangzhou <wangzhou@shengqugames.com>
2020-09-29 14:09:30 +0800

    [geek-algorithm-leetcode] StateMachine

--- tech-geek-component/src/main/java/com/frankcooper/StatemachineApplication.java
+++ tech-geek-component/src/main/java/com/frankcooper/StatemachineApplication.java
@@ -13,6 +13,7 @@ import com.frankcooper.spring.stateMachine.turnstile.TurnstileStates;
  * https://www.jianshu.com/p/1a4947a099e9?from=singlemessage
  * https://blog.csdn.net/guo_xl/article/details/83716342
  * https://docs.spring.io/spring-statemachine/docs/2.0.3.BUILD-SNAPSHOT/reference/htmlsingle/#statemachine-examples-persist
+ * https://www.jianshu.com/p/4b07b76ca90d
  */
 @SpringBootApplication
 public class StatemachineApplication implements CommandLineRunner {
