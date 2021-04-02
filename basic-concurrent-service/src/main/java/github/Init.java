0d69f6dd4195c2834c17604c31b1c62e6b1c4abc
wat1r <cnwangzhou@hotmail.com>
2021-04-02 09:43:15 +0800

    trap water

--- basic-concurrent-service/src/main/java/github/Init.java
+++ basic-concurrent-service/src/main/java/github/Init.java
@@ -1,9 +1,22 @@
 package github;
 
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+
 /**
  * Created by FrankCooper
  * Date 2019/2/9 12:11
  * Description
  */
 public class Init {
+
+    public static void main(String[] args) {
+        ArrayList<String> list = Lists.newArrayList("1");
+        for (int i = list.size() - 1; i >= 0; i--) {
+
+        }
+        String.format("%d\n", 1);
+
+    }
 }
