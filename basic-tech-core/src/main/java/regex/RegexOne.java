d73139b2555657fb7c1625c9ffbc45b088f32fb4
wat1r <cnwangzhou@hotmail.com>
2021-04-26 23:42:34 +0800

    Regex

--- basic-tech-core/src/main/java/regex/RegexOne.java
+++ basic-tech-core/src/main/java/regex/RegexOne.java
@@ -8,22 +8,17 @@ public class RegexOne {
     static RegexOne handler = new RegexOne();
 
     public static void main(String[] args) {
-//        handler.hasSensitiveSymbol(" ddd");
-        Pattern pattern = Pattern.compile("^(?!(_|-|\\.))([a-zA-Z0-9_|-]|(\\.){0,1}).*(?<!(\\.|-|_))");
+        Pattern pattern = Pattern.compile("^(?!(_|-|\\.))([a-zA-Z0-9_-]|(\\.){0,1}).*(?<!(\\.|-|_))");
 //        pattern = Pattern.compile("^[A-Za-z0-9]{1}[A-Za-z0-9_-]*?\\.?[A-Za-z0-9_-]*?[A-Za-z0-9]{1}$");
-//        pattern = Pattern.compile("^(?![^.]*?\\.[^.]*?\\.)[a-zA-Z][a-zA-Z\\d_.-]*$");
-        pattern = Pattern.compile("^(?![^.]*?\\.[^.]*?\\.)[a-zA-Z][a-zA-Z\\d_.-].*(?<!(\\.|-|_))");
-        pattern =Pattern.compile("^(?![\\._\\-])[0-9a-zA-Z_\\-]*\\.?[0-9a-zA-Z_\\-]*(?<![\\._\\-])$");
-        String[] strs = {"a123456888_", "a123456*", "Ua123456.", "Ua1.23.456", "asdf.as.dfa", "Ua1_2-3456","Ua1大23.456"};
+//        pattern = Pattern.compile("^(?![^.]*?\\.[^.]*?\\.)[a-zA-Z][a-zA-Z\\d_\\.-]*?(?<!(\\.|-|_))");
+        pattern = Pattern.compile("^(?![\\._-])[a-zA-Z0-9_-]*\\.?[a-zA-Z0-9_-]*?(?<![\\._-])$");
+        String[] strs = {"&a123456888", "a123456888_", "a123456 *", "Ua123456.", "Ua1.23456", "asdf.as.dfa", "as.df.as.dfa", "Ua1_2-3456", "Ua1大23.456"};
         for (String str : strs) {
             handler.process(str, pattern);
         }
-
     }
 
-
     public boolean process(String str, Pattern pattern) {
-//        Pattern pattern = Pattern.compile("[\\s\\\\/:\\*\\?\\\"<>\\|]");
         Matcher matcher = pattern.matcher(str);
         if (matcher.matches()) {
             System.out.printf("%s--->%s\n", str, "true");
