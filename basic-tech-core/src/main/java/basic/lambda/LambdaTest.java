fd2c47ed3cb72ab4bb585524e79becd702a67e2b
wangzhou <wangzhou@shengqugames.com>
2021-08-05 21:00:44 +0800

    daily

--- basic-tech-core/src/main/java/basic/lambda/LambdaTest.java
+++ basic-tech-core/src/main/java/basic/lambda/LambdaTest.java
@@ -11,7 +11,9 @@ public class LambdaTest {
 
     public static void main(String[] args) {
 //        filterMap();
-        buildList();
+//        buildList();
+//        forEachMap();
+        removeItem();
     }
 
 
@@ -46,6 +48,33 @@ public class LambdaTest {
         List<String> list = IntStream.range(0, size).mapToObj(k -> "?").collect(Collectors.toList());
         List<String> list1 = Collections.nCopies(size, "?");
         System.out.println("end");
+    }
+
+
+    private static void forEachMap() {
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println("ddd");
+        }
+        map.forEach((k, v) -> System.out.println(k));
+    }
+
+    private static void removeItem() {
+        HashMap<Integer, String> map = new HashMap<Integer, String>() {
+            {
+                put(1, "1");
+                put(2, "2");
+                put(3, "3");
+            }
+        };
+//        map.forEach((k, v) -> {
+//            if (k == 1) map.remove(k);
+//        });
+        Iterator<Map.Entry<Integer, String>> it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            if (it.next().getKey() == 1) it.remove();
+        }
+        map.forEach((k, v) -> System.out.printf("%d->%s\n", k, v));
     }
 
 
