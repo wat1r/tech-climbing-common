b97cfd3f2b2ea7621876b1f98ed9c57204bc64dc
wangzhou <wangzhou@shengqugames.com>
2021-08-13 16:51:06 +0800

    draw.io

---
+++ basic-tech-core/src/main/java/algorithm/enterprise/MeituanProcessor.java
@@ -0,0 +1,54 @@
+package algorithm.enterprise;
+
+
+import java.util.*;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/**
+ * @author: wangzhou(Frank Cooper)
+ * @date: 2021/8/13 8:27
+ * @description:
+ *
+ * 关联链接：https://blog.csdn.net/wat1r/article/details/119669619
+ */
+
+public class MeituanProcessor {
+
+    //小美的用户名
+    static class _1st {
+
+
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            int T = scanner.nextInt();
+            String[] arr = new String[T];
+            for (int i = 0; i < T; i++) {
+                arr[i] = scanner.next();
+            }
+            String regex = "^([a-zA-Z])([a-zA-Z]*)([0-9]{1,})([a-zA-Z0-9]*)$";
+//        String regex = "^([a-zA-Z])([a-zA-Z]*)([0-9]+)([a-zA-Z]*)$";
+//        String regex = "^([a-zA-Z])([a-zA-Z]*)([0-9]+)+([a-zA-Z0-9]*)$";
+            for (int i = 0; i < T; i++) {
+                System.out.printf("%s\n",arr[i]);
+                boolean res = Pattern.matches(regex, arr[i]);
+                if (res) System.out.println("Accept");
+                else System.out.println("Wrong");
+            }
+
+        }
+
+    }
+
+
+    static class _2nd{
+
+        public static void main(String[] args) {
+
+        }
+    }
+
+}
