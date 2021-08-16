d8885844f08378c6339c9ccb0e7bd53cb2513e4a
wat1r <cnwangzhou@hotmail.com>
2021-08-16 09:46:38 +0800

    daily

--- basic-tech-core/src/main/java/algorithm/enterprise/MeituanProcessor.java
+++ basic-tech-core/src/main/java/algorithm/enterprise/MeituanProcessor.java
@@ -10,17 +10,13 @@ import java.util.regex.Pattern;
 /**
  * @author: wangzhou(Frank Cooper)
  * @date: 2021/8/13 8:27
- * @description:
- *
- * 关联链接：https://blog.csdn.net/wat1r/article/details/119669619
+ * @description: 关联链接：https://blog.csdn.net/wat1r/article/details/119669619
  */
 
 public class MeituanProcessor {
 
     //小美的用户名
     static class _1st {
-
-
         public static void main(String[] args) {
 
             Scanner scanner = new Scanner(System.in);
@@ -33,7 +29,7 @@ public class MeituanProcessor {
 //        String regex = "^([a-zA-Z])([a-zA-Z]*)([0-9]+)([a-zA-Z]*)$";
 //        String regex = "^([a-zA-Z])([a-zA-Z]*)([0-9]+)+([a-zA-Z0-9]*)$";
             for (int i = 0; i < T; i++) {
-                System.out.printf("%s\n",arr[i]);
+                System.out.printf("%s\n", arr[i]);
                 boolean res = Pattern.matches(regex, arr[i]);
                 if (res) System.out.println("Accept");
                 else System.out.println("Wrong");
@@ -43,10 +39,21 @@ public class MeituanProcessor {
 
     }
 
-
-    static class _2nd{
+    //小美的仓库整理
+    static class _2nd {
 
         public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int n = scanner.nextInt();
+            int[] weights = new int[n];
+            for (int i = 0; i < n; i++) {
+                weights[i] = scanner.nextInt();
+            }
+            int[] orders = new int[n];
+            for (int i = 0; i < n; i++) {
+                orders[i] = scanner.nextInt();
+            }
+            //process
 
         }
     }
