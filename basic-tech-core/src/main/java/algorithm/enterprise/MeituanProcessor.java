32a37efd6c0601ca87965648ef971e9f7ecc20c9
wat1r <cnwangzhou@hotmail.com>
2021-08-17 09:56:13 +0800

    daily

--- basic-tech-core/src/main/java/algorithm/enterprise/MeituanProcessor.java
+++ basic-tech-core/src/main/java/algorithm/enterprise/MeituanProcessor.java
@@ -91,6 +91,32 @@ public class MeituanProcessor {
         }
     }
 
+
+    //搭配出售
+    static class _11th {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt(), d = sc.nextInt();
+            int e = sc.nextInt(), f = sc.nextInt(), g = sc.nextInt();
+            List<int[]> list = new ArrayList<int[]>() {{
+                add(new int[]{e, a});
+                add(new int[]{f, b});
+                add(new int[]{g, c});
+            }};
+            list.sort(((o1, o2) -> o2[0] - o1[0]));
+            long res = 0;
+            for (int i = 0; i < list.size(); i++) {
+                int[] cur = list.get(i);
+                int value = cur[0];
+                int cnt = Math.min(cur[1], d);
+                res += (long) value * cnt;
+                d -= cnt;
+            }
+            System.out.printf("%d", res);
+        }
+    }
+
+
     //偏爱字母
     static class _13th {
         public static void main(String[] args) {
