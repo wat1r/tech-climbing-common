30290fd514dba640ea691c440909e6d144df5d41
wangzhou <wangzhou@shengqugames.com>
2021-11-11 20:46:30 +0800

    1.linux

---
+++ basic-concurrent-service/src/main/java/basic/concurrent/Test.java
@@ -0,0 +1,27 @@
+package basic.concurrent;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.*;
+
+/**
+ * @author: wangzhou(Frank Cooper)
+ * @date: 2021/11/11 15:31
+ * @description:
+ */
+public class Test {
+
+//    private static ThreadFactory nameFactory = new ThreadFactoryBuilder()
+//            .setNameFormat("demo-pool-%d").build();
+//
+//    private static ExecutorService pool = new ThreadPoolExecutor(5, 200,
+//            0L, TimeUnit.MILLISECONDS,
+//            new LinkedBlockingQueue<Runnable>(1024), namedFactory, new ThreadPoolExecutor.AbortPolicy());
+//
+//    public static void main(String[] args) {
+//
+//        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+////            pool.execute(new SubThread());
+//        }
+//    }
+}
