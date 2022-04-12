81888fa41eb9b7a6662aadf545fcb13202d8bc33
wat1r <cnwangzhou@hotmail.com>
2022-04-12 23:01:12 +0800

    daily

--- basic-tech-core/src/main/java/algorithm/leetcode/case1/RepeativeCaseIV.java
+++ basic-tech-core/src/main/java/algorithm/leetcode/case1/RepeativeCaseIV.java
@@ -50,12 +50,47 @@ public class RepeativeCaseIV {
 //    winter
 
 
+    static class _1st {
+        public static void main(String[] args) {
+            _1st handler = new _1st();
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt(), k = sc.nextInt();
+            String s = sc.next();
+            process(n, k, s);
+        }
+
+
+        private static void process(int n, int k, String s) {
+            Map<Integer, Integer> map = new HashMap<>();
+            map.put(0, 1);
+            List<Integer> list = new ArrayList<>();
+            int res = 0;
+            for (int i = 0; i < n; i++) {
+                list.add(s.charAt(i) - '0');
+                int t = rmod(list, k);
+                if (map.containsKey(t)) res += map.get(t);
+                map.put(t, map.getOrDefault(t, 0) + 1);
+            }
+            System.out.println(res);
+
+        }
+
+        //求模的函数
+        private static int rmod(List<Integer> list, int k) {
+            int res = 0;
+            for (int x : list) {
+                res = res * 10 + x;
+                res %= k;
+            }
+            return res;
+        }
+
+    }
+
 
 //    int calculateDaysBetweenDates(){
 //
 //    }
-
-
 
 
 }
