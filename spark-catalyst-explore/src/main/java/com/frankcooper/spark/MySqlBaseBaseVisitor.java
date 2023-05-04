99afb34d99b06be1c2b8410acbaa0c9168053a7e
wangzhou <wangzhou@shengqugames.com>
2023-05-04 20:16:54 +0800

    init spark-catalyst-explore

--- spark-catalyst-explore/src/main/java/com/frankcooper/spark/MySqlBaseBaseVisitor.java
+++ spark-catalyst-explore/src/main/java/com/frankcooper/spark/MySqlBaseBaseVisitor.java
@@ -1,5 +1,6 @@
 package com.frankcooper.spark;
 
+import org.antlr.v4.runtime.tree.ParseTree;
 import org.apache.spark.sql.catalyst.parser.SqlBaseBaseVisitor;
 import org.apache.spark.sql.catalyst.parser.SqlBaseParser;
 
@@ -45,5 +46,58 @@ public class MySqlBaseBaseVisitor extends SqlBaseBaseVisitor<Void> {
         return null;
     }
 
+    @Override
+    public Void visitInsertOverwriteTable(SqlBaseParser.InsertOverwriteTableContext ctx) {
+        System.out.println("==============5==============");
+        System.out.println(ctx.getText());
+        for (int i = 0; i < ctx.getChildCount(); i++) {
+            ParseTree child = ctx.getChild(i);
+            System.out.println(child.getText());
+            visit(child);
+        }
+        return null;
+    }
 
+    @Override
+    public Void visitCreateTable(SqlBaseParser.CreateTableContext ctx) {
+        System.out.println("==============6==============");
+        System.out.println(ctx.getText());
+        return null;
+    }
+
+    @Override
+    public Void visitCreateHiveTable(SqlBaseParser.CreateHiveTableContext ctx) {
+        System.out.println("==============7==============");
+        System.out.println(ctx.getText());
+//        for (int i = 0; i < ctx.getChildCount(); i++) {
+//            ParseTree child = ctx.getChild(i);
+//            System.out.println(child.getText());
+//            visit(child);
+//
+//        }
+        visit(ctx.query());
+        return null;
+    }
+
+    @Override
+    public Void visitCreateView(SqlBaseParser.CreateViewContext ctx) {
+        System.out.println("==============8==============");
+        System.out.println(ctx.getText());
+        return null;
+    }
+
+    @Override
+    public Void visitNamedQuery(SqlBaseParser.NamedQueryContext ctx) {
+        System.out.println("==============9==============");
+        System.out.println(ctx.getText());
+        return null;
+    }
+
+    @Override
+    public Void visitQuery(SqlBaseParser.QueryContext ctx) {
+        System.out.println("==============10==============");
+        System.out.println(ctx.getText());
+        visit(ctx.queryNoWith());
+        return null;
+    }
 }
--- spark-catalyst-explore/src/main/java/com/frankcooper/spark/MySqlBaseTest.java
+++ spark-catalyst-explore/src/main/java/com/frankcooper/spark/MySqlBaseTest.java
@@ -15,6 +15,8 @@ import java.util.function.Function;
 public class MySqlBaseTest {
     public static void main(String[] args) {
         String sql = "WITH gun_list AS ( SELECT mid, count(DISTINCT character_id) AS character_nums, CASE WHEN count(DISTINCT character_id) > 1 THEN count(DISTINCT character_id) - 1 ELSE 0 END AS gun_times FROM dw.gmqstsea_character_login_glog WHERE part_date BETWEEN '2023-03-30' AND '2023-04-02' AND area_id IN (10001) GROUP BY mid ) SELECT * FROM gun_list";
+        sql = "insert overwrite table dm.data_product_dim partition(part_date='2023-04-06')  select a.product_id, a.product_name, a.product_content, a.product_price  from temp.data_product_dim a";
+//        sql = "CREATE TABLE temp.ajie_weixin AS SELECT mobile FROM dw.data_callback_log WHERE callback_channel_name = '人工微信' AND to_date(callback_time) BETWEEN '2023-04-01' AND '2023-04-31' AND callback_team IN (2, 3, 4) AND PART_DATE <= '2023-05-03'";
         process("statement", sql, SqlBaseParser::singleStatement);
 
     }
